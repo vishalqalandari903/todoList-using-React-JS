11f9dd04875d57cd299c267f3c9beeae8d5a6001
Vishal Kumar <vishalfreelancing903@gmail.com>
2024-02-26 17:42:58 +0500

    checkbox styling

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -33,13 +33,13 @@ function TodoItem({ todo }) {
 
   return (
     <div
-      className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
+      className={`flex align-middle justify-center border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
         todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
       }`}
     >
       <input
         type="checkbox"
-        className="cursor-pointer"
+        className="cursor-pointer todo_msg_checkbox self-center"
         checked={todo.completed}
         onChange={() => {
           if (isTodoEditable) {
