460804556474a4b32c2ef18b8d2e9063cdee650b
Vishal Kumar <vishalfreelancing903@gmail.com>
2024-02-26 17:54:04 +0500

    adding responsiveness

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -61,7 +61,7 @@ function TodoItem({ todo }) {
       {/* Edit, Save Button */}
       <button
         id="save_todo_msg_btn"
-        className=" h-full aspect-square inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
+        className=" h-full aspect-square inline-flex lg:h-8 lg:w-8 md:h-8 md:w-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
         onClick={() => {
           if (todo.completed) return;
 
